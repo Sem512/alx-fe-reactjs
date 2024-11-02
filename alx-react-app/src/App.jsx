c98ca9209baa35c88f566fd33e66d324229d14c5
Semir Sultan Mohammed <semi512113@gmail.com>
2024-11-02 13:02:40 +0300

    added userprofile.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,14 +6,12 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import MainContent from './components/MainContent.jsx'
+import UserProfile from './components/UserProfile.jsx'
 
 function App(){
   return(
-    <div class="Main">
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
+    
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
 
   );
 }

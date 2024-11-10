84aaee8861662b11136e7590ee9bb529f13c7201
Semir Sultan Mohammed <semi512113@gmail.com>
2024-11-10 14:17:05 +0300

    added counter.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,15 +7,11 @@ import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import MainContent from './components/MainContent.jsx'
 import UserProfile from './components/UserProfile.jsx'
+import Counter from './components/Counter.jsx'
 
 function App(){
   return(
-    <main>
-      <Header />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <MainContent />
-      <Footer />
-    </main>
+    <Counter />
   );
 }
 

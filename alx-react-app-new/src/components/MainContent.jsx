4ab74faf86d4037ec428e4cf900fb9ec181de8bd
Semir Sultan Mohammed <semi512113@gmail.com>
2024-11-10 14:12:22 +0300

    added second react app

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent(){
+    return(
+<main style={{opacity:'0.9'}}>
+  <p style={{color:'greenyellow'}}>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+    );
+}
+
+export default MainContent;

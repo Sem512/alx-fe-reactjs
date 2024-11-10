ab522f6c5ea9095384be7fa3f3dd9be5745f3924
Semir Sultan Mohammed <semi512113@gmail.com>
2024-11-10 15:36:38 +0300

    navbar.jsx modifications

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{display:'flex',flexDirection:'column',justifyContent:'space-evenly',alignItems:'flex-start'}}>
+    <nav style={{display:'flex',flexDirection:'column',justifyContent:'space-evenly',alignItems:'flex-start',backgroundColor:'beige'}}>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>

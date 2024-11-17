19778ccbebca4c5dbbd144bc5d2dbe3c65c970b7
Semir Sultan Mohammed <semi512113@gmail.com>
2024-11-17 19:42:26 +0300

    added Recipe store

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,12 @@
+import RecipeList from './components/recipeList';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Recipe Manager</h1>
+      <RecipeList />
+    </div>
+  );
+};
+
+export default App;

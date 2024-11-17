4a5c99cd699a0319f75083b22f0ec75b427e7814
Semir Sultan Mohammed <semi512113@gmail.com>
2024-11-17 20:21:52 +0300

    set

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
-  recipes: [],
+  setRecipes: [],
 
   // Add a new recipe
   addRecipe: (recipe) => set((state) => ({

d01f5a4b7cd8b0675fc265e5bd72b3ce6a392f00
Semir Sultan Mohammed <semi512113@gmail.com>
2024-12-01 17:43:54 +0300

    todo list

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 import TodoList from '../components/TodoList';
 
 test('renders TodoList component', () => {
--- react-todo/src/components/AddTodoForm.js
+++ react-todo/src/components/AddTodoForm.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 
 const AddTodoForm = ({ newTodo, setNewTodo, addTodo }) => {
   return (

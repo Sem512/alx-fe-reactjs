ba19f37f974a6c217b0736e35d491b3ab535d606
Semir Sultan Mohammed <semi512113@gmail.com>
2024-12-01 18:06:52 +0300

    tailwind

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
       <img 
         src="https://via.placeholder.com/150" 
         alt="User" 
-        className="rounded-full sm:w-24 h-24 md:w-36 sm:h-36 mx-auto"
+        className="rounded-full sm:w-24 sm:h-24 md:w-36 sm:h-36 mx-auto"
       />
       <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-sm sm:text-sm md:text-base text-gray-600">

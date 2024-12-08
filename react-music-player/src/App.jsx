aaf4acc0e1e4efdc5930ba7c84c917ebeba8282d
Semir Sultan Mohammed <semi512113@gmail.com>
2024-12-08 18:52:32 +0300

    modified app.jsx

--- react-music-player/src/App.jsx
+++ react-music-player/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,21 +7,15 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://brandingforum.org/wp-content/uploads/2023/10/spotify-logo-1024x651.webp" target="_blank">
+          <img src="https://brandingforum.org/wp-content/uploads/2023/10/spotify-logo-1024x651.webp" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Spotify API Music Player</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Coming in {count} days
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more

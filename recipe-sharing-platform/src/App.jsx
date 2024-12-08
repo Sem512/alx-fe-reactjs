1998c0937bfd08bf536328e6ebbf1519b468726c
Semir Sultan Mohammed <semi512113@gmail.com>
2024-12-08 18:28:03 +0300

    renamed recipe form

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import HomePage from './components/HomePage'
 import RecipeDetail from './components/RecipeDetail'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AddRecipeForm from './components/AddRecipe'
+import AddRecipeForm from './components/AddRecipeForm'
 
 function App() {
   const [count, setCount] = useState(0)
